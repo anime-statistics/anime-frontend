a14bd14d412a359056f98c7facebe16e502999e2
nvd05 <nvd05@bk.ru>
2024-12-26 21:33:48 +0500

    cleared to the foundation

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,7 +1,8 @@
 
 import { ReportHandler } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+
+export const reportWebVitals = (onPerfEntry?: ReportHandler) => {
 	if (onPerfEntry && onPerfEntry instanceof Function) {
 		import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
 			getCLS(onPerfEntry);
@@ -12,5 +13,3 @@ const reportWebVitals = (onPerfEntry?: ReportHandler) => {
 		});
 	}
 };
-
-export default reportWebVitals;
