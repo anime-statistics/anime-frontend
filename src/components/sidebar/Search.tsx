8e79524fbc761a819b4afa1e167bd71d9d54847a
nvd05 <nvd05@bk.ru>
2024-12-31 00:51:31 +0500

    added feedback components\sidebar

--- src/components/sidebar/Search.tsx
+++ src/components/sidebar/Search.tsx
@@ -2,17 +2,36 @@
 import { FloatingLabel, Form, InputGroup } from 'react-bootstrap';
 
 
-export function Search ()
+export interface SearchProps
+{
+	setType : React.Dispatch<React.SetStateAction<string>>;
+	type    : string;
+
+	setSearch : React.Dispatch<React.SetStateAction<string>>;
+	search    : string;
+}
+
+export function Search ({ setType, type, setSearch, search }: SearchProps)
 {
 	return (
 		<InputGroup>
-			<Form.Select title='Тип работы'>
+			<Form.Select
+				title = 'Тип работы'
+
+				onChange = { ev => setType(ev.target.value) }
+				value    = { type }
+			>
 				<option>Аниме</option>
 				<option>Манга</option>
 			</Form.Select>
 
 			<FloatingLabel label='Найти по названию'>
-				<Form.Control placeholder='Пять невест' />
+				<Form.Control
+					placeholder = 'Пять невест'
+
+					onChange = { ev => setSearch(ev.target.value) }
+					value    = { search }
+				/>
 			</FloatingLabel>
 		</InputGroup>
 	);
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -8,6 +8,9 @@ import { Search } from './Search';
 
 export function Sidebar ()
 {
+	const [ type,   setType   ] = useState('');
+	const [ search, setSearch ] = useState('');
+
 	const [ data, setData ] = useState([]);
 
 	const next = function (): void
@@ -16,7 +19,7 @@ export function Sidebar ()
 
 	return (
 		<Stack as='aside' gap={2} className='bg-white p-2 rounded'>
-			<Search />
+			<Search {...{setType, type, setSearch, search}} />
 			<Catalog {...{next, data}} />
 		</Stack>
 	);
