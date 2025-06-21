a0d5e648e110540a394c7c52ae0a1548385d5280
nvd05 <nvd05@bk.ru>
2025-06-22 01:56:10 +0500

    Update TypeConverter.tsx

--- src/tools/converters/TypeConverter.tsx
+++ src/tools/converters/TypeConverter.tsx
@@ -9,6 +9,9 @@ export function TypeConverter ({ data }: TypeConverterProps)
 	switch (data)
 	{
 		case 'tv': return <>TV Сериал</>;
+
+		// PG-13 - Детям до 13 лет просмотр не желателен
+		case 'pg_13': return <>PG-13</>;
 	}
 
 	return <>{ data }</>;
